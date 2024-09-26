8d0a6ef615b8bd785ac32da1c810bd1c376750fc
moyzaldi <60549010+moyzaldi@users.noreply.github.com>
2024-09-26 20:05:29 +0300

    Ex7: Запросы и методы

--- compare_query_type.py
+++ compare_query_type.py
@@ -3,7 +3,7 @@ import requests
 methods = ['', 'OPTIONS','POST', 'GET', 'PUT', 'DELETE']
 
 for method in methods:
-    response = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type', params={'method': f"{method}"})
+    response = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type', data={'method': f"{method}"})
     print(f"Если для POST используем метод: {method}, то ответ: ", response.status_code, response.text)
 print('******************')
 
@@ -13,11 +13,11 @@ for method in methods:
 print('******************')
 
 for method in methods:
-    response = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type', params={'method': f"{method}"})
+    response = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type', data={'method': f"{method}"})
     print(f"Если для PUT используем метод: {method}, то ответ: ", response.status_code, response.text)
 print('******************')
 
 for method in methods:
-    response = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type', params={'method': f"{method}"})
+    response = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type', data={'method': f"{method}"})
     print(f"Если для DELETE используем метод: {method}, то ответ: ", response.status_code, response.text)
 print('******************')
