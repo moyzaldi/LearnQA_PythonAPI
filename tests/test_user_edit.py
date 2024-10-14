df424cd0e44a407c4ef4b076c017fb58c7f7bf70
moyzaldi <60549010+moyzaldi@users.noreply.github.com>
2024-10-14 23:06:59 +0300

    Ex17: Негативные тесты на PUT

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,10 +1,12 @@
 from lib.my_requests import MyRequests
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
+import random
 
 class TestUserEdit(BaseCase):
-    # REGISTER
+
     def test_edit_just_created_user(self):
+        # REGISTER
         register_data =  self.prepare_registration_date()
         response1 = MyRequests.post("/user/", data=register_data)
 
@@ -12,12 +14,10 @@ class TestUserEdit(BaseCase):
         Assertions.assert_json_has_key(response1, "id")
 
         email =  register_data['email']
-        first_name = register_data ['firstName']
         password = register_data['password']
         user_id= self.get_json_value(response1,"id")
 
         #LOGIN
-
         login_data = {
             'email': email,
             'password': password
@@ -27,7 +27,6 @@ class TestUserEdit(BaseCase):
         token = self.get_header(response2, 'x-csrf-token')
 
         #EDIT
-
         new_name =  "Changed Name"
         response3 = MyRequests.put( f"/user/{user_id}",
         headers = {"x-csrf-token": token},
@@ -45,7 +44,127 @@ class TestUserEdit(BaseCase):
         Assertions.assert_json_value_by_name (response4, "firstName", new_name, "Wrong name of the user after edit")
 
 
+    def test_edit_user_without_auth(self):
+        user_id = 2
+
+        response = MyRequests.put( f"/user/{user_id}",
+        data = {"firstName": "Changed Name"}
+         )
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            'utf-8') == '{"error":"Auth token not supplied"}', f"Unexpected content {response.content}"
 
 
+    def test_edit_user_with_auth_by_another_user(self):
+        # REGISTER_1
+        register_data = self.prepare_registration_date()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, 'auth_sid')
+        token = self.get_header(response2, 'x-csrf-token')
+
+        # REGISTER_2
+        register_data_2 = self.prepare_registration_date()
+        response3 = MyRequests.post("/user/", data=register_data_2)
+
+        Assertions.assert_code_status(response3, 200)
+        Assertions.assert_json_has_key(response3, "id")
+
+        user_id = self.get_json_value(response3, "id")
+
+        # EDIT
+
+        new_name = "Changed Name"
+        response4 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={'auth_sid': auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response4, 400)
+        assert response4.content.decode(
+            'utf-8') == '{"error":"This user can only edit their own data."}', f"Unexpected content {response4.content}"
+
+    def test_edit_user_email_with_auth_without_symbol(self):
+        # REGISTER
+        register_data = self.prepare_registration_date()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        first_name = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, 'auth_sid')
+        token = self.get_header(response2, 'x-csrf-token')
+
+        # EDIT
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={'auth_sid': auth_sid},
+                                   data={"email": 'test.test.com'}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+        assert response3.content.decode(
+            'utf-8') == '{"error":"Invalid email format"}', f"Unexpected content {response3.content}"
+
+
+    def test_edit_user_firstName_to_short_value_with_auth(self):
+        # REGISTER
+        register_data = self.prepare_registration_date()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        first_name = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, 'auth_sid')
+        token = self.get_header(response2, 'x-csrf-token')
+
+        # EDIT
+        firstName = str(random.randint(1, 9))
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={'auth_sid': auth_sid},
+                                   data={"firstName":  firstName}
+                                   )
+
+        Assertions.assert_code_status(response3, 400)
+        assert response3.content.decode(
+            'utf-8') == '{"error":"The value for field `firstName` is too short"}', f"Unexpected content {response3.content}"
 
 
