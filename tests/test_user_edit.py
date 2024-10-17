7416140366a7c718e065d2b53bb23e2cabbb4281
moyzaldi <60549010+moyzaldi@users.noreply.github.com>
2024-10-17 22:37:18 +0300

    Ex17: Негативные тесты на PUT

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -3,6 +3,7 @@ from lib.assertions import Assertions
 from lib.base_case import BaseCase
 import random
 
+
 class TestUserEdit(BaseCase):
 
     def test_edit_just_created_user(self):
@@ -27,6 +28,7 @@ class TestUserEdit(BaseCase):
         token = self.get_header(response2, 'x-csrf-token')
 
         #EDIT
+
         new_name =  "Changed Name"
         response3 = MyRequests.put( f"/user/{user_id}",
         headers = {"x-csrf-token": token},
@@ -67,28 +69,29 @@ class TestUserEdit(BaseCase):
         email = register_data['email']
         password = register_data['password']
 
+        # REGISTER_2
+        register_data_2 = self.prepare_registration_date()
+        response2 = MyRequests.post("/user/", data=register_data_2)
+
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+
+        user_id = self.get_json_value(response2, "id")
+
 
         # LOGIN
         login_data = {
             'email': email,
             'password': password
         }
-        response2 = MyRequests.post("/user/login", data=login_data)
-        auth_sid = self.get_cookie(response2, 'auth_sid')
-        token = self.get_header(response2, 'x-csrf-token')
-
-        # REGISTER_2
-        register_data_2 = self.prepare_registration_date()
-        response3 = MyRequests.post("/user/", data=register_data_2)
-
-        Assertions.assert_code_status(response3, 200)
-        Assertions.assert_json_has_key(response3, "id")
-
-        user_id = self.get_json_value(response3, "id")
+        response3 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response3, 'auth_sid')
+        token = self.get_header(response3, 'x-csrf-token')
 
         # EDIT
 
         new_name = "Changed Name"
+
         response4 = MyRequests.put(f"/user/{user_id}",
                                    headers={"x-csrf-token": token},
                                    cookies={'auth_sid': auth_sid},
@@ -122,6 +125,7 @@ class TestUserEdit(BaseCase):
         token = self.get_header(response2, 'x-csrf-token')
 
         # EDIT
+
         response3 = MyRequests.put(f"/user/{user_id}",
                                    headers={"x-csrf-token": token},
                                    cookies={'auth_sid': auth_sid},
@@ -156,6 +160,7 @@ class TestUserEdit(BaseCase):
         token = self.get_header(response2, 'x-csrf-token')
 
         # EDIT
+
         firstName = str(random.randint(1, 9))
         response3 = MyRequests.put(f"/user/{user_id}",
                                    headers={"x-csrf-token": token},
